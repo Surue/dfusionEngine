5dfa6c7bde3f034df96870359ff842f54e7fd897
Surue <nicolas06062000@hotmail.com>
2021-11-16 23:18:41 +0100

    Fix gameloop using correct timer

--- engine/core/engine/src/gameloop.cpp
+++ engine/core/engine/src/gameloop.cpp
@@ -1,37 +1,40 @@
-#include "gameloop.h"
-
+#include <gameloop.h>
 #include <log.h>
 
 #include <chrono>
 #include <string>
 #include <thread>
+#include <vector>
 
 namespace dfe {
-Gameloop::Gameloop() : _elapsedTime(0), _isRunning(false) {}
+Gameloop::Gameloop() : _elapsedTime(0), _isRunning(false), _deltaTime(0.0) {}
 void Gameloop::Init() {}
 void Gameloop::Update() {
   _isRunning = true;
 
-  auto startChrono = std::chrono::system_clock::now();
-  std::chrono::system_clock::time_point startFrame;
+  auto startChrono = std::chrono::high_resolution_clock::now();
+  std::chrono::high_resolution_clock::time_point startFrame;
+  
   while (_isRunning) {
-    startFrame = std::chrono::system_clock::now();
-    // Update
+    // Time start frame
+    startFrame = std::chrono::high_resolution_clock::now();
 
-    Debug::Log(std::to_string(_elapsedTime));
+    // Update
+    // TODO Inputs
+
+    // TODO Update
+
+    // TODO Render
 
     // Wait time
-    //auto t = std::chrono::duration<double>(std ::chrono::system_clock::now() - startFrame).count();
-    auto sleepDuration = std::chrono::duration<double>(
-        0.02 - std::chrono::duration<double>(std ::chrono::system_clock::now() -
-                                             startFrame)
-                   .count());
-
-    std::this_thread::sleep_for(sleepDuration);
+    while (std::chrono::duration<double>(
+               std::chrono::high_resolution_clock::now() - startFrame)
+               .count() < 0.02) {
+    }
 
     // Update timers
     _deltaTime = std::chrono::duration<double>(
-                     std::chrono::system_clock::now() - startFrame)
+                     std::chrono::high_resolution_clock::now() - startFrame)
                      .count();
 
     _elapsedTime += _deltaTime;
