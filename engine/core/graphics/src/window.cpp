8f46db6590a285cdbd83e0117323cf551100d340
Surue <nicolas06062000@hotmail.com>
2021-11-28 21:46:38 +0100

    Window uses Vulkan

--- engine/core/graphics/src/window.cpp
+++ engine/core/graphics/src/window.cpp
@@ -1,12 +1,14 @@
 #include <window.h>
+#include <SDL_vulkan.h>
 
 namespace dfe {
 Window::Window(std::string_view windowName)
     : _windowName(windowName), _window(nullptr) {}
 
 void Window::Init() {
-  _window = SDL_CreateWindow(&_windowName.data()[0], SDL_WINDOWPOS_UNDEFINED,
-                             SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE );
+  _window = SDL_CreateWindow(&_windowName.data()[0], SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+      640, 480,
+      SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
 }
 
 void Window::Destroy() { SDL_DestroyWindow(_window); }
