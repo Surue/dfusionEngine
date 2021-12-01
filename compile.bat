58bb472d166fa9328c316829bf9a1ece54023d65
Surue <nicolas06062000@hotmail.com>
2021-12-01 22:36:49 +0100

    Add compiler for shader HLSL to SPIR-V

---
+++ compile.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.2.148.1\Bin32\glslc.exe resources\shaders\default.vert -o resources\shaders\default.vert.spv
+C:\VulkanSDK\1.2.148.1\Bin32\glslc.exe resources\shaders\default.frag -o resources\shaders\default.frag.spv
+pause
